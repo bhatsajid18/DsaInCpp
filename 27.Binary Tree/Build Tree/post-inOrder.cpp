29d38d4d2f9186e6ed30406a484e8d0f16c8c4a9
bhatsajid18 <bhatsajid425@gmail.com>
2023-08-13 15:54:00 +0530

    modified naming error

--- 27.Binary Tree/Build Tree/post-inOrder.cpp
+++ 27.Binary Tree/Build Tree/post-inOrder.cpp
@@ -23,14 +23,14 @@ int search(int inorder[],int start,int end,int curr){
     return -1;
 }
 
-Node* build(int preorder[],int inorder[],int start, int end,int size){
+Node* build(int postorder[],int inorder[],int start, int end,int size){
     //start and end are of inorder array
     //size is array size or no. of nodes in tree
     static int idx=size;
     if (start>end){
         return NULL;
     }
-    int curr=preorder[idx];
+    int curr=postorder[idx];
     idx--;
     Node* n=new Node(curr);
 
@@ -40,8 +40,8 @@ Node* build(int preorder[],int inorder[],int start, int end,int size){
     
     int pos=search(inorder,start,end,curr);
 
-    n->right=build(preorder,inorder,pos+1,end,size);
-    n->left=build(preorder,inorder,start,pos-1,size);
+    n->right=build(postorder,inorder,pos+1,end,size);
+    n->left=build(postorder,inorder,start,pos-1,size);
 
     return n;
 }
@@ -56,10 +56,10 @@ void inOrderPrint(struct Node* root){
 }
 
 int main(){
-    int preorder[]={4,2,5,3,1};
+    int postorder[]={4,2,5,3,1};
     int inorder[]={4,2,1,5,3};
 
-    Node* root=build(preorder,inorder,0,4,4);
+    Node* root=build(postorder,inorder,0,4,4);
 
     inOrderPrint(root);
     cout<<endl;
